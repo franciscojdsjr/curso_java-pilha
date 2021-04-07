c3a6af19e3a9d9ec0818c0d586a7dc46eddd6495
Francisco Júnior <franciscojdsjr@gmail.com>
2021-04-07 20:30:36 -0300

    att

--- src/MinhaExcecao.java
+++ src/MinhaExcecao.java
@@ -1,8 +1,10 @@
 
-public class MinhaExcecao extends Exception{ //checked
-	
+public class MinhaExcecao extends Exception { // checked
+
 	public MinhaExcecao(String msg) {
 		super(msg);
 	}
+
+	// teste
 
 }
